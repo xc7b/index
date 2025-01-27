25d90225dd8fead59f830062d693a42592b665f6
b7cx <oxycodeineinc@gmail.com>
2025-01-27 08:34:19 -0600

    Update table.lua

--- table.lua
+++ table.lua
@@ -1,14 +1,19 @@
-script_key = "" --// key here
 getgenv().Index = {
     ["Start"] = {
-        ["IntroBlur"] = { ['Active'] = true },
+        ["Options"] = { 
+            ['Intro'] = true,
+            ['License'] = '', -- [[ place your luarmor key here ]]
+        },
         ["Silent"] = {
-            ['Type'] = 'Target', --// Fov, Target
-            ['ClosestPart'] = true,
-            ['TargetParts'] = 'HumanoidRootPart',
+            ['Type'] = 'Target', -- [[ FOV, Target ]]
+            ['Prediction'] = 0.02,
+            ['TargetParts'] = 'Head',
             ['Air'] = 'Head',
-            ['Prediction'] = 0,
-            ['Fov'] = {
+            ['ClosestPart'] = false,
+            ['ClosestPoint'] = true,
+            ['ClosestPointMode'] = 'Full', -- [[ Partial, Full ]]
+            ['AntiAimViewer'] = true,
+            ['FOV'] = {
                 ['Transparency'] = 1,
                 ['Visible'] = false,
                 ['Thickness'] = 1,
@@ -16,33 +21,64 @@ getgenv().Index = {
                 ['Radius'] = 200,
             },
         },
-        ["TriggerBot"] = {
-            ['Notification'] = true,
-            ['Delay'] = 0.0001,
-            ['Blacklisted'] = { "[Knife]" },
-            ['Keybind'] = 'T',
+        ["SilentOffsets"] = { 
+            ['Jump'] = -0.15, 
+            ['Fall'] = 0 
         },
-        ["SilentOffsets"] = { ['Jump'] = 0, ['Fall'] = 0 },
-        ["AimbotOffsets"] = { ['Active'] = true, ['Jump'] = 0, ['Fall'] = 0.125 },
+        ["AimbotOffsets"] = { 
+            ['Active'] = true, 
+            ['Jump'] = 4, 
+            ['Fall'] = 0.125 
+        },
         ["AimBot"] = {
-            ['Notification'] = false,
-            ['Keybind'] = 'C',
             ['Active'] = true,
-            ['Predictions'] = 0.19,
-            ['Smoothness'] = 0.15,
+            ['Keybind'] = 'C',
+            ['Smoothness'] = 1,
+            ['Predictions'] = 0.18,
             ['TargetParts'] = 'Head',
+            ['ClosestPart'] = false,
+            ['Notification'] = false,
+        },
+        ["Style"] = {
+            ['Easing'] = 'Elastic', -- [[ Linear, Sine, Quad, Cubic, Exponential, Back, Bounce, Elastic ]]
+            ['Direction'] = 'InOut', -- [[ In, Out, InOut ]]
+        },
+        ['HitboxExpander'] = {
+            ['Enabled'] = true,
+            ['Visualize'] = true,
+            ['NormalSize'] = 15,     
+            ['Scaling'] = { -- [[ custom scaling if u dont want to use the same value for each x,y,z ]]
+                ['Enabled'] = false, -- [[ if "false" then uses "normalsize" ]]
+                ['X'] = 11,
+                ['Y'] = 1,
+                ['Z'] = 1
+            },
         },
         ["MouseTp"] = {
             ['Active'] = true,
-            ['LerpValues'] = 0.2, --// the higher the better the tp is
-            ['MousePredictions'] = 0.15,
+            ['LerpValues'] = 1,
+            ['MousePredictions'] = 0.1,
+        },
+        ["TriggerBot"] = {
+            ['Enabled'] = true,
+            ['Keybind'] = 'T',
+            ['Delay'] = 0.0001, -- [[ this is literally fucking Prediction ]]
+            ['Notification'] = true,
+            ['Blacklisted'] = { -- [[ can add any weapon / item to this such as food, katana and other things]]
+                "[Knife]"
+            },
         },
         ["Misc"] = {
+            ['Skybox'] = {
+                ['Enabled'] = true, -- [[ broken ]]
+                ['SkyboxID'] = 'rbxassetid://106428636831184', -- [[ default is index skybox ]]
+            }
             ['Resolver'] = {
-                ['Keybind'] = 'G',
                 ['Active'] = false,
-                ['Notifications'] = false,
+                ['Keybind'] = 'G',
+                ['Mode'] = 'Delta', -- [[ Delta, Recalculate ]]
                 ['Adjust'] = 0.2,
+                ['Notifications'] = false,
             },
             ['Adjustment'] = {
                 ['VelocityThresold'] = 100,
@@ -51,17 +87,31 @@ getgenv().Index = {
                 ['Keybind'] = 'X',
                 ['Active'] = true,
                 ['Acceleration'] = 0.0,
-                ['Variety'] = 'Third', --// first
+                ['Variety'] = 'Third', -- First, Third
             },
             ['Spin'] = {
-                ['Keybind'] = 'V',
+                ['Keybind'] = 'Z',
                 ['Degrees'] = 360,
-                ['Acceleration'] = 5000,
+                ['Acceleration'] = 4000,
                 ['Directions'] = 'North',
                 ['Smoothness'] = 1,
             },
+            ['WalkSpeed'] = {
+                ['Active'] = true,
+                ['Mode'] = 'Humanoid', -- [[ Humanoid, CFrame ]] [[ cframe is NOT added yet ]]
+                ['Speed'] = 100,
+                ['Keybind'] = 'H' 
+            },
+            ['NoJumpCoolDown'] = {
+                ['Active'] = true,
+            },
+            ['Checks'] = {
+                ['KO'] = true,
+                ['Wall'] = true,
+                ['ForceField'] = true,
+            }
         },
     },
 }
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a0ca193dc0855c82f5e849a21f49d320.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xc7b/index/refs/heads/main/loader.lua"))()

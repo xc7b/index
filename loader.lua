5ee7cc9289caf690e0dd74028f704fcde2b34d50
b7cx <oxycodeineinc@gmail.com>
2025-02-17 17:23:49 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-if getgenv().IndexLoaded then
+if getgenv().Index then
     script_key = getgenv().Index.Start.Options.License
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c3b91e872b1b9db6e796733164bd81cf.lua"))()
 end

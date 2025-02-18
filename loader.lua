6ac7f8a8c726c5a8f7fda9fd8ebfeae74a173e9e
b7cx <oxycodeineinc@gmail.com>
2025-02-18 17:35:23 -0600

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if getgenv().Index then
+    script_key = getgenv().Index.Start.Options.License
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c3b91e872b1b9db6e796733164bd81cf.lua"))()
+end

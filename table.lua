220eefab584c8d5fe3da2b5a29e34a5166c32bed
b7cx <oxycodeineinc@gmail.com>
2025-02-22 20:31:53 -0600

    Update table.lua

--- table.lua
+++ table.lua
@@ -1,122 +1,123 @@
 getgenv().Index = {
     ['Start'] = {
         ['Options'] = { 
-            Intro = true,
-            Version = '2.4.1',
-            License = 'key', -- [[ luarmor key ]]
+            ['Intro'] = true,
+            ['Version'] = '2.4.2',
+            ['License'] = 'keygoeshere', -- [[ luarmor key ]]
         },
         ['Silent'] = {
-            Type = 'Target', -- [[ FOV, Target ]]
-            Prediction = 0,
-            Hitpart = 'UpperTorso',
-            AirPart = 'Head',
-            ClosestPart = true,
-            NearestPoint = true,
-            NearestPointMode = 'Full', -- [[ Partial, Full ]]
-            AntiAimViewer = true,
+            ['Enabled'] = true,
+            ['Type'] = 'Target', -- [[ FOV, Target ]]
+            ['Prediction'] = 0.0325,
+            ['Hitpart'] = 'Head',
+            ['AirPart'] = 'Head',
+            ['ClosestPart'] = false,
+            ['NearestPoint'] = false,
+            ['NearestPointMode'] = 'Full', -- [[ Partial, Full ]]
+            ['AntiAimViewer'] = false,
             ['FOV'] = {
-                Transparency = 1,
-                Visible = false,
-                Thickness = 1,
-                Color = Color3.fromRGB(111, 111, 11),
-                Radius = 200,
+                    ['Transparency'] = 1,
+                    ['Visible'] = false,
+                    ['Thickness'] = 1,
+                    ['Color'] = Color3.fromRGB(111, 111, 11),
+                    ['Radius'] = 200,
             },
         },
         ['SilentOffsets'] = { 
-            Jump = 0, 
-            Fall = 0
+            ['Jump'] = 0, 
+            ['Fall'] = 0
         },
         ['AimbotOffsets'] = { 
-            Enabled = true, 
-            Jump = 0, 
-            Fall = 0.125
+            ['Enabled'] = true, 
+            ['Jump'] = 0, 
+            ['Fall'] = 0
         },
         ['Aimbot'] = {
-            Enabled = true,
-            Keybind = 'C',
-            Smoothness = 0.158,
-            Predictions = 0.1,
-            Hitpart = 'Head',
-            ClosestPart = false,
-            Notification = false,
+            ['Enabled'] = true,
+            ['Keybind'] = 'E',
+            ['Smoothness'] = 1,
+            ['Prediction'] = 0.1,
+            ['Hitpart'] = 'Head',
+            ['ClosestPart'] = false,
+            ['Notification'] = false
         },
         ['Style'] = {
-            Easing = 'Linear', -- [[ Linear, Sine, Quad, Cubic, Exponential, Back, Bounce, Elastic ]]
-            Direction = 'In', -- [[ In, Out, InOut ]]
+            ['Easing'] = 'Sine', -- [[ Linear, Sine, Quad, Cubic, Exponential, Back, Bounce, Elastic ]]
+            ['Direction'] = 'Out' -- [[ In, Out, InOut ]]
         },
         ['HitboxExpander'] = {
-            Enabled = false,
-            Visualize = false,
-            NormalSize = 15,     
+            ['Enabled'] = false,
+            ['Visualize'] = false,
+            ['NormalSize'] = 15,     
             ['Scaling'] = { -- [[ custom scaling if u dont want to use the same value for each x,y,z ]]
-                Enabled = false, -- [[ if "false" then uses "normalsize" ]]
-                X = 11,
-                Y = 1,
-                Z = 1
+                    ['Enabled'] = false, -- [[ if "false" then uses "normalsize" ]]
+                    ['X'] = 11,
+                    ['Y'] = 1,
+                    ['Z'] = 1
             },
         },
         ['MouseTp'] = {
-            Enabled = true,
-            LerpValues = 0.25,
-            MousePredictions = 0.1,
+            ['Enabled'] = true,
+            ['LerpValues'] = 1,
+            ['MousePrediction'] = 0.1
         },
         ['TriggerBot'] = {
-            Enabled = false,
-            Keybind = 'T',
-            Delay = 0.0001, -- [[ this is literally fucking Prediction ]]
-            Notification = true,
+            ['Enabled'] = false,
+            ['Keybind'] = 'T',
+            ['Delay'] = 0.0001, -- [[ this is literally fucking Prediction ]]
+            ['Notification'] = true,
             ['Blacklisted'] = { -- [[ can add any weapon / item to this such as food, katana and other things]]
-                "[Knife]"
+                   "[Knife]"
             },
         },
         ["Misc"] = {
             ['Skyboxes'] = {
-                Enabled = true, -- [[ enable / disable switch is broken currently ]] 
-                SkyboxTexture = 'Minecraft', -- [[ Default, Vaporwave, Redshift, Desert, DaBaby, Minecraft, SpongeBob, Skibidi, Blaze, Pussy Cat, Among Us, Space Wave, Space Wave2, Turquoise Wave, Dark Night, Bright Pink, White Galaxy ]] | credits tsol  my best friend in skidding (temporary till i feel like making a skybox ) 
+                ['Enabled'] = false, -- [[ enable / disable switch is broken currently ]] 
+                ['SkyboxTexture'] = 'Minecraft', -- [[ Default, Vaporwave, Redshift, Desert, DaBaby, Minecraft, SpongeBob, Skibidi, Blaze, Pussy Cat, Among Us, Space Wave, Space Wave2, Turquoise Wave, Dark Night, Bright Pink, White Galaxy ]] | credits tsol  my best friend in skidding (temporary till i feel like making a skybox ) 
             },
             ['Textures'] = {
-                Enabled = true,
-                Material = 'Rock', -- [[ https://create.roblox.com/docs/reference/engine/enums/Material ]]
-                UseColor = false,
-                Color = Color3.fromRGB(255,255,255),
+                ['Enabled'] = false,
+                ['Material'] = 'Concrete', -- [[ https://create.roblox.com/docs/reference/engine/enums/Material ]]
+                ['UseColor'] = false,
+                ['Color'] = Color3.fromRGB(255,255,255),
             },
             ['Resolver'] = {
-                Enabled = false,
-                Keybind = 'G',
-                Method = 'Delta', -- [[ Delta, Recalculate ]]
-                Adjust = 0.2,
-                Notifications = false,
+                ['Enabled'] = false,
+                ['Keybind'] = 'G',
+                ['Method'] = 'Delta', -- [[ Delta, Recalculate ]]
+                ['Adjust'] = 0.2,
+                ['Notifications'] = false,
             },
             ['Adjustment'] = {
-                VelocityThresold = 100,
+                ['VelocityThresold'] = 100,
             },
             ['Macro'] = {
-                Keybind = 'X',
-                Enabled = true,
-                Acceleration = 0.0,
-                Variety = 'Third', -- First, Third
+                ['Keybind'] = 'X',
+                ['Enabled'] = true,
+                ['Acceleration'] = 0.0,
+                ['Variety'] = 'Third', -- First, Third
             },
             ['Spin'] = {
-                Keybind = 'V',
-                Degrees = 360,
-                Acceleration = 4900,
-                Directions = 'North',
-                Smoothness = 1,
+                ['Keybind'] = 'V',
+                ['Degrees'] = 360,
+                ['Acceleration'] = 4900,
+                ['Directions'] = 'North',
+                ['Smoothness'] = 1,
             },
             ['WalkSpeed'] = {
-                Enabled = true,
-                Mode = 'Humanoid', -- [[ Humanoid, CFrame ]] [[ cframe is NOT added yet ]]
-                Speed = 100,
-                Keybind = 'H' 
+                ['Enabled'] = true,
+                ['Mode'] = 'Humanoid', -- [[ Humanoid, CFrame ]] [[ cframe is NOT added yet ]]
+                ['Speed'] = 165,
+                ['Keybind'] = 'H' 
             },
             ['NoJumpCoolDown'] = {
-                Enabled = true,
+                ['Enabled'] = true,
             },
             ['Checks'] = {
-                KO = true,
-                Visible = true,
-                Grabbed = true
-            }
+                ['KO'] = true,
+                ['Visible'] = true,
+                ['Grabbed'] = true
+            },
         },
     },
 }

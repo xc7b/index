0f97262e1054e33b370bfc4a4846c455a9b7cb39
b7cx <oxycodeineinc@gmail.com>
2025-03-10 02:20:58 -0500

    Update table.lua

--- table.lua
+++ table.lua
@@ -1,27 +1,27 @@
 getgenv().Index = {
     ['Start'] = {
         ['Options'] = { 
-            ['Intro'] = { ['Enabled'] = true, ['Use Sound'] = true, },
+            ['Intro'] = { ['Enabled'] = true, ['Use Sound'] = false, },
             ['Execution Information'] = false,
-            ['Version'] = '2.6',
-            ['License'] = 'keygoeshere', -- [[ luarmor key ]]
+            ['Version'] = ('2.7'),
+            ['License'] = ('keygoeshere'), --[[ luarmor key ]]--
         },
         ['Silent'] = {
             ['Enabled'] = true,
-            ['Type'] = 'FOV', -- [[ FOV, Target ]]
+            ['Type'] = 'Target', --[[ FOV, Target ]]--
             ['Prediction'] = 0,
             ['Hitpart'] = 'UpperTorso',
             ['AirPart'] = 'Head',
-            ['ClosestPart'] = true,
+            ['ClosestPart'] = false,
             ['Nearest Point'] = false,
-            ['Nearest Point Mode'] = 'Full', -- [[ Scalar, Dynamic, Generic, Full ]]
+            ['Nearest Point Mode'] = 'Full', --[[ Generic, Static, Scalar, Dynamic, Full ]]--
             ['Anti Aim Viewer'] = false,
             ['FOV'] = {
                     ['Transparency'] = 1,
                     ['Visible'] = false,
                     ['Thickness'] = 1,
                     ['Color'] = Color3.fromRGB(111, 111, 11),
-                    ['Radius'] = 75,
+                    ['Radius'] = 150,
             },
         },
         ['SilentOffsets'] = { 
@@ -29,72 +29,71 @@ getgenv().Index = {
             ['Fall'] = 0
         },
         ['AimbotOffsets'] = { 
-            ['Enabled'] = false, 
+            ['Enabled'] = true, 
             ['Jump'] = 3, 
             ['Fall'] = -12
         },
         ['Aimbot'] = {
             ['Enabled'] = true,
-            ['Keybind'] = 'E',
-            ['Smoothness'] = 0.03,
-            ['Prediction'] = 0.1,
+            ['Keybind'] = 'C',
+            ['Smoothness'] = 0.257,
+            ['Prediction'] = 0.14,
             ['Hitpart'] = 'UpperTorso',
             ['ClosestPart'] = false,
             ['Notification'] = false
         },
         ['Style'] = {
-            ['Easing'] = 'Linear', -- [[ Linear, Sine, Quad, Cubic, Exponential, Back, Bounce, Elastic ]]
-            ['Direction'] = 'In' -- [[ In, Out, InOut ]]
+            ['Easing'] = 'Linear', --[[ Linear, Sine, Quad, Cubic, Exponential, Back, Bounce, Elastic ]]--
+            ['Direction'] = 'In' --[[ In, Out, InOut ]]--
         },
         ['HitboxExpander'] = {
             ['Enabled'] = false,
             ['Visualize'] = false,
             ['NormalSize'] = 15,     
-            ['Scaling'] = { -- [[ custom scaling if u dont want to use the same value for each x,y,z ]]
-                    ['Enabled'] = false, -- [[ if "false" then uses "normalsize" ]]
+            ['Scaling'] = { --[[ custom scaling if u dont want to use the same value for each x,y,z ]]--
+                    ['Enabled'] = false, --[[ if "false" then uses "normalsize" ]]--
                     ['X'] = 11,
                     ['Y'] = 1,
                     ['Z'] = 1
             },
         },
         ['MouseTp'] = {
-            ['Enabled'] = false,
+            ['Enabled'] = true,
             ['LerpValues'] = 0.341941515,
             ['MousePrediction'] = 0.18014512
         },
         ['TriggerBot'] = {
             ['Enabled'] = false,
             ['Keybind'] = 'T',
-            ['Prediction'] = 0.01, -- [[ also known as delay ]]
+            ['Prediction'] = 0.01, --[[ also known as delay ]]--
             ['Notification'] = true,
-            ['Blacklisted'] = { -- [[ can add any weapon / item to this such as food, katana and other things]]
+            ['Blacklisted'] = { --[[ can add any weapon / item to this such as food, katana and other things]]--
                    "[Knife]"
             },
         },
-        ["Misc"] = {
+        ['Misc'] = {
+            ['Adjustment'] = {
+                ['VelocityThresold'] = 100,
+            },
             ['Spread Modifier'] = {
                 ['Enabled'] = true,
-                ['Value'] = 100, -- [[ 100 for no spread modification ]]
+                ['Value'] = 100, --[[ 100 for no spread modification ]]--
             },
-            ['Skyboxes'] = {
-                ['Enabled'] = true, -- [[ enable / disable switch is broken currently ]] 
-                ['SkyboxTexture'] = 'Dark Night', -- [[ Default, Vaporwave, Redshift, Desert, DaBaby, Minecraft, SpongeBob, Skibidi, Blaze, Pussy Cat, Among Us, Space Wave, Space Wave2, Turquoise Wave, Dark Night, Bright Pink, White Galaxy ]] | credits tsol  my best friend in skidding (temporary till i feel like making a skybox ) 
+            ['WalkSpeed'] = {
+                ['Enabled'] = true,
+                ['Mode'] = 'Humanoid', --[[ Humanoid, CFrame ]]--
+                ['Speed'] = 275,
+                ['Keybind'] = 'H',
             },
-            ['Textures'] = {
-                ['Enabled'] = false,
-                ['Material'] = 'Concrete', -- [[ https://create.roblox.com/docs/reference/engine/enums/Material ]]
-                ['UseColor'] = false,
-                ['Color'] = Color3.fromRGB(255,255,255),
+            ['No Jump Cool Down'] = {
+                ['Enabled'] = true,
             },
             ['Resolver'] = {
                 ['Enabled'] = false,
                 ['Keybind'] = 'G',
-                ['Method'] = 'Delta', -- [[ Delta, Recalculate ]]
+                ['Method'] = 'Delta', --[[ Delta, Recalculate ]]--
                 ['Adjust'] = 0.2,
                 ['Notifications'] = false,
-            },
-            ['Adjustment'] = {
-                ['VelocityThresold'] = 100,
             },
             ['Macro'] = {
                 ['Keybind'] = 'X',
@@ -109,14 +108,15 @@ getgenv().Index = {
                 ['Directions'] = 'North',
                 ['Smoothness'] = 1,
             },
-            ['WalkSpeed'] = {
+            ['Textures'] = {
                 ['Enabled'] = true,
-                ['Mode'] = 'Humanoid', -- [[ Humanoid, CFrame ]] [[ cframe is NOT added yet ]]
-                ['Speed'] = 100,
-                ['Keybind'] = 'H' 
+                ['Material'] = 'Brick', -- [[ https://create.roblox.com/docs/reference/engine/enums/Material ]]
+                ['UseColor'] = false,
+                ['Color'] = Color3.fromRGB(255,255,255),
             },
-            ['NoJumpCoolDown'] = {
-                ['Enabled'] = true,
+            ['Skyboxes'] = {
+                ['Enabled'] = true, -- [[ enable / disable switch is broken currently ]] 
+                ['SkyboxTexture'] = 'Dark Night', -- [[ Default, Vaporwave, Redshift, Desert, DaBaby, Minecraft, SpongeBob, Skibidi, Blaze, Pussy Cat, Among Us, Space Wave, Space Wave2, Turquoise Wave, Dark Night, Bright Pink, White Galaxy ]] | credits tsol  my best friend in skidding (temporary till i feel like making a skybox ) 
             },
             ['Checks'] = {
                 ['KO'] = true,
@@ -125,5 +125,6 @@ getgenv().Index = {
         },
     },
 }
+
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xc7b/index/refs/heads/main/loader.lua"))()
